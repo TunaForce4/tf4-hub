7f4ffd73b8279ba52f2d316b5dfab5040e178d69
dinahland <hsunnies@gmail.com>
2025-08-15 15:31:26 +0900

    feat: Hub Entity 생성

---
+++ src/main/java/com/tunaforce/hub/entity/Hub.java
@@ -0,0 +1,41 @@
+package com.tunaforce.hub.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "p_hub")
+public class Hub {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long hubId;
+
+    @Column(nullable = false)
+    private String hubName;
+
+    @Column(nullable = false)
+    private String hubAddress;
+
+    private Double hubLatitude;
+
+    private Double hubLongitude;
+
+    private Long hubAdminId;
+
+    @Builder
+    public Hub(String hubName, String hubAddress, Double hubLatitude, Double hubLongitude) {
+        this.hubName = hubName;
+        this.hubAddress = hubAddress;
+        this.hubLatitude = hubLatitude;
+        this.hubLongitude = hubLongitude;
+    }
+
+    public void setHubAdminId(Long hubAdminId) {
+        this.hubAdminId = hubAdminId;
+    }
+}
